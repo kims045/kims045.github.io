3ceacb2f141a679b18768f3bd6c71553c03a4deb
Chloe Kim <kims045@newschool.edu>
2017-08-28 17:38:30 -0400

    dsaf

---
+++ Blogtest/assets/js/main.js
@@ -0,0 +1,20 @@
+// var one = document.getElementsByClassName('one')[0];
+// var text = document.getElementsByClassName('text')[0];
+
+// one.onmouseover = function(){
+// 	text.style.display = 'block';
+// };
+// one.onmouseout = function(){
+// 	text.style.display = 'hidden';
+// };
+
+
+
+// $(function() {
+// 	$('.one').hover(function() {
+// 		$('.text').css('display', 'block');
+// 	}, function(){
+// 		$('.text').css('display', 'none');
+// 	});
+// });
+
